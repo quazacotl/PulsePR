b6c2608a1d17571815bca1fe7f83a2b4af83ed0a
Vagrant <89261815645@ya.ru>
2020-04-06 18:32:22 +0300

    Validated

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ $(document).ready(function(){
             $('.pageup').fadeOut();
         }
     });
-    $("a[href^='#']").click(function(){
+    $("a[href='#up']").click(function(){
         var _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
